5ac18aa3a5b5fa2b1c84a77505bec2ce731a0fe0
CahyaW06 <yudhacahyawijaya@gmail.com>
2025-01-10 00:37:36 +0700

    update footer

--- resources/views/base/footer.blade.php
+++ resources/views/base/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
-      <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © 2024. All rights reserved.</span>
+      <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Developed by Yudha Cahya Wijaya</span>
     </div>
   </footer>
