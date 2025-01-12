0de5e89f52fb574ce6bf527ce41a7be257237c3c
CahyaW06 <yudhacahyawijaya@gmail.com>
2025-01-12 11:30:34 +0700

    update total bill text

--- resources/views/log/transaksi/create.blade.php
+++ resources/views/log/transaksi/create.blade.php
@@ -12,7 +12,7 @@
         <form class="forms-sample" action="{{ route('log.keuangan.store') }}" method="POST">
           @csrf
           <div id="form-container">
-            <div class="d-md-flex gap-2 align-items-center mt-3" id="row-input">
+            <div class="d-lg-flex gap-2 align-items-center mt-3 border-bottom pb-4 pb-lg-0" id="row-input">
               <div class="form-group">
                 <label for="retail">Retail</label>
                 <div class="d-flex flex-column">
@@ -33,10 +33,10 @@
                   </select>
                 </div>
               </div>
-              <div class="form-group">
+              <div class="form-group status-form">
                 <label for="status">Status Transaksi</label>
                 <div class="d-flex flex-column">
-                  <select class="form-control form-control-sm text-black" name="status[]" id="status">
+                  <select class="form-control form-control-sm text-black status" name="status[]" id="status">
                     <option value="1">Laku</option>
                     <option value="2">Ganti Rugi</option>
                   </select>
@@ -48,14 +48,17 @@
               </div>
               <div class="form-group">
                 <label for="nominal">Nominal</label>
-                <input type="integer" class="form-control form-control-sm" name="nominal[]" id="nominal" placeholder="Nominal" required>
+                <input type="integer" class="form-control form-control-sm nominal" name="nominal[]" id="nominal" placeholder="Nominal" required>
               </div>
               <button type="button" class="btn btn-danger btn-sm remove-row ms-1 d-flex align-items-center"><i class="mdi mdi-delete"></i><span class="ms-1">Hapus</span></button>
             </div>
           </div>
-          <div class="d-flex gap-1">
-            <button type="button" id="add-row" class="btn btn-warning mt-3">Tambah</button>
-            <button type="submit" class="btn btn-success mt-3">Simpan</button>
+          <div class="d-md-flex gap-2 align-items-baseline justify-content-between mt-5 mt-lg-0">
+            <span class="">Total: <span class="display-5 text-bg-success px-3 rounded ms-1">Rp <span id="total-nota"></span></span></span>
+            <div class="d-flex gap-1 mt-2">
+              <button type="button" id="add-row" class="btn btn-warning mt-3">Tambah</button>
+              <button type="submit" class="btn btn-success mt-3">Simpan</button>
+            </div>
           </div>
         </form>
       </div>
@@ -67,6 +70,15 @@
     $(document).ready(function () {
       var rowCount = 1; // Menyimpan jumlah baris yang ada
 
+      // Fungsi untuk update total-nominal
+      function updateTotal() {
+        let total = 0;
+        $(".nominal").each(function () {
+          if ($(this).parent().siblings(".status-form").find(".status").val() == 1) {total += parseInt($(this).val().replace(/[.]/g, ''));}
+        });
+        $("#total-nota").text(total.toLocaleString());
+      }
+
       // Fungsi untuk membuat ID unik
       function updateIds(row, count) {
           row.find('#log_konsinyasi-1').attr('id', 'log_konsinyasi-' + count);
@@ -75,7 +87,13 @@
           row.find('#jumlah-1').attr('id', 'jumlah-' + count);
       }
 
-      // Fungsi untuk update option
+      // Fungsi ketika update option
+      function whenStatusChanged() {
+        $(".status").change(function (e) {
+          e.preventDefault();
+          updateTotal();
+        });
+      }
 
       // Tambahkan event listener untuk format angka
       function formatNumberInputs() {
@@ -86,6 +104,8 @@
                       var n = parseInt($(this).val().replace(/\D/g,''),10);
                       $(this).val(n.toLocaleString());
                   }
+
+                  updateTotal();
               });
           });
       }
@@ -97,10 +117,13 @@
           updateIds(newRow, rowCount); // Update ID untuk row baru
           $('#form-container').append(newRow);
           formatNumberInputs(); // Tambahkan kembali event listener ke input baru
+          updateTotal();
+          whenStatusChanged();
       });
 
       // Menghapus row saat tombol 'Hapus' diklik
       $(document).on('click', '.remove-row', function () {
+          updateTotal();
           if ($('.remove-row').length > 1) {
               $(this).closest('#row-input').remove(); // Hapus row
           } else {
@@ -110,6 +133,7 @@
 
       // Terapkan format angka pada input jumlah dan nominal
       formatNumberInputs();
+      whenStatusChanged();
   });
 </script>
 @endsection
